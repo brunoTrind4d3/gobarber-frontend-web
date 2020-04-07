bd506a0cc0fc872c37da3beb2f5c0279a1693069
Bruno Trindade <b.l.trindade@outlook.com>
2020-04-07 17:46:44 -0300

    fix: config in wrong place

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { ToastContainer } from 'react-toastify';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import GlobalStyle from './styles/global';
-import Routes from './routes';
-import history from './services/history';
+
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+import history from './services/history';
+
 import { store, persistor } from './store';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
