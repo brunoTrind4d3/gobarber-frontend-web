d5124e16b3a798dc08525f4f56800c60b0a42b31
Bruno Trindade <b.l.trindade@outlook.com>
2020-04-07 14:53:24 -0300

    Style signIn and signUp pages

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
-
-export default function SignUp() {
-  return <h1>SignUp</h1>;
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho uma conta</Link>
+      </form>
+    </>
+  );
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,41 +1,32 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-
-
-  *{
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+  * {
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
   }
-
-  *:focus{
+  *:focus {
     outline: 0;
   }
-
-  html, body, #root{
+  html, body, #root {
     height: 100%;
   }
-
-  body{
+  body {
     -webkit-font-smoothing: antialiased;
   }
-
-  body, input, button{
+  body, input, button {
     font: 14px 'Roboto', sans-serif;
   }
-
-  a{
+  a {
     text-decoration: none;
   }
-
-  ul{
+  ul {
     list-style: none;
   }
-
-  button{
+  button {
     cursor: pointer;
   }
 `;
